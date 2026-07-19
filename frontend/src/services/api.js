5ec62443e8f5027f44e42d29147fe6244964c1b2
Devapriya06 <devapriya200607@gmail.com>
2026-07-19 22:58:09 +0530

    Updated backend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://tourist-places-explorer-l61c.onrender.com/api",
+  baseURL: "https://tourist-places-explorer-rs8z.onrender.com/api",
 });
